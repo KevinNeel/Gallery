c95e2551d76ea83f6287f15efeb9544972e59b64
Kevin <kevinneel007@gmail.com>
2021-07-16 21:10:14 +0530

    update

--- src/db_conn/db_conn.js
+++ src/db_conn/db_conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:127.0.0.1:27017/Gallery',{
+mongoose.connect('mongodb://localhost:27017/Gallery',{
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
