f7bee17c97688cce73ed2140922b383bb7d59a29
Kevin <kevinneel007@gmail.com>
2021-07-17 19:10:54 +0530

    update

--- src/db_conn/db_conn.js
+++ src/db_conn/db_conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Gallery',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Gallery', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
